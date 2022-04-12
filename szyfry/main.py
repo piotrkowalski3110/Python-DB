4e21cb27211a16764ce244968d88056b89980376
Piotr Kowalski <piotr.kowalski3110@gmail.com>
2022-04-12 11:15:06 +0200

    test

--- szyfry/main.py
+++ szyfry/main.py
@@ -5,23 +5,52 @@ def encode (znaki, lista):
     for n in range (length):
         if n < len(lista):
             ascii = ord(znaki[n])
-            ascii = ascii + lista[n]
-            if ascii > 90:
+            ascii = ascii + (lista[n]%26)
+            if ascii>90:
                 ascii = ascii - 26
             znak = chr(ascii)
             print(znak, end="")
 
         if n >= len(lista):
             ascii = ord(znaki[n])
-            ascii = ascii + lista[licznik]
+            ascii = ascii + (lista[licznik])%26
             if ascii > 90:
                 ascii = ascii - 26
             znak = chr(ascii)
             print(znak, end="")
-            licznik = licznik +1
+            licznik = licznik + 1
 
             if licznik >= len(lista):
                 licznik = 0
 
-encode("KOMPUTER", [50])
+def decode(znaki, lista):
+    length = len(znaki)
+    licznik = 0
+
+    for n in range (length):
+        if n < len(lista):
+            ascii = ord(znaki[n])
+            ascii = ascii - (lista[n]%26)
+            if ascii<65:
+                ascii = ascii + 26
+            znak = chr(ascii)
+            print(znak, end="")
+
+        if n >= len(lista):
+            ascii = ord(znaki[n])
+            ascii = ascii - (lista[licznik])%26
+            if ascii < 65:
+                ascii = ascii + 26
+            znak = chr(ascii)
+            print(znak, end="")
+            licznik = licznik + 1
+
+            if licznik >= len(lista):
+                licznik = 0
+
+
+
+encode("KOMPUTER", [17, 3, 18])
+print("")
+decode("BREGXLVU", [17, 3, 18])
 
