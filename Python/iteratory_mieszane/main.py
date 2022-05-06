7405c1071c64570ee471dab6c3277e601e101ffb
pkowalski3110 <piotr.kowalski3110@gmail.com>
2022-05-06 13:18:11 +0200

    iteratory mieszane

---
+++ Python/iteratory_mieszane/main.py
@@ -0,0 +1,23 @@
+class MyNumbers:
+
+    def __init__(self, n):
+        self.n = n
+        self.i = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.n == self.i:
+            raise StopIteration
+
+        self.i += 1
+        if not self.i % 3:
+            return 'HAHA'
+        elif not self.i % 5:
+            return 'HEHE'
+        else:
+            return self.i
+
+for x in MyNumbers(20):
+  print(x)
